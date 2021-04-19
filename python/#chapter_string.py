83170235e101e6f4207fd975d053b1a89ef50db9
Joshuasun11 <longbowroy007@gmail.com>
2021-04-18 21:19:27 -0700

    practicing

--- python/#chapter_string.py
+++ python/#chapter_string.py
@@ -1,6 +1,15 @@
 #chapter_string.py
+'''
 weekstr="星期一星期二星期三星期四星期五星期六星期天"
 dayinput=eval(input("please input number:"))
 pos=(dayinput-1)*3
 print(pos)
 print(weekstr[pos:(pos+3)]) #tempstr[a:b] the start position is a the ending position is b-13
+'''
+'''
+for i in range(12):
+    print(chr(9800+i),end='\n')
+    
+'''
+"{0:-^20}".format("fuckyou")
+
