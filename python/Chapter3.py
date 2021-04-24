2744aa2584f5a4db78c4d2d6c05a2322abb5f418
Joshuasun11 <longbowroy007@gmail.com>
2021-04-24 16:20:00 -0700

    new

--- python/Chapter3.py
+++ python/Chapter3.py
@@ -19,7 +19,23 @@ for i in range(scale+1): #scale = 10 but it starts counting from 0
     time.sleep(0.1)
 print("------end processing------")
 '''
+'''
 import time
 for i in range(101):
     print("\r{:3} %".format(i),end=" ")
     time.sleep(0.1)
+'''
+import time
+scale=50            #可以任意定义
+print("start processing".center(scale,"-"))         #.center() function
+start = time.perf_counter()             #cpu start time counting
+for i in range(scale+1):
+    a = (i/scale)*100
+    #print(a)
+    b = '*' * i
+    c = '.' * (scale-i)
+    dur=time.perf_counter()-start           #time consumption of every percent counting
+    print("\r [{}-{:^4.0f}%->{}] {:.1f}s".format(b,a,c,dur),end='')           #槽位，end=''同行结束后不换行并带字符''，\r光标回到此行头部
+    time.sleep(0.1)
+print("\n"+"end processing".center(scale,'-'))          #two prints can be combined into one prints by using "+"
+
